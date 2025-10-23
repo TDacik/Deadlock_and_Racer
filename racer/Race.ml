22f40f91157e352a120da60b81b7f0dcac391131
Tomáš Dacík <xdacik00@stud.fit.vutbr.cz>
2025-10-23 14:21:46 +0200

    [Racer] add reason for may data race

--- racer/Race.ml
+++ racer/Race.ml
@@ -1,13 +1,12 @@
 (* Representation of data races.
  *
- * TODO: add reason to May race
  * TODO: clean up race checking
  *
  * Author: Tomas Dacik (idacik@fit.vut.cz), 2024 *)
 
 open MemoryAccess
 
-type race_kind = Must | May
+type race_kind = Must | May of string
 
 type t = {
   kind : race_kind;
@@ -17,10 +16,10 @@ type t = {
 }
 
 (** Check whether a pair of accesses is a race *)
-let is_race_base threads parallel combine_lss self other =
+let is_may_race threads parallel self other =
   let open ThreadAnalysis in
-  let ls1 = combine_lss self.locksets in
-  let ls2 = combine_lss other.locksets in
+  let ls1 = Lock.PowerSet.flatten_inter self.locksets in
+  let ls2 = Lock.PowerSet.flatten_inter other.locksets in
   let thread1 = get_thread self in
   let thread2 = get_thread other in
   let stmt1 = get_stmt self in
@@ -71,26 +70,6 @@ let is_alloced_array base = match base with
   | Base.Allocated (var, _, validity) -> Cil.isArrayType var.vtype
   | _ -> false
 
-let is_may_race threads parallel self other =
-  is_race_base threads parallel Lock.PowerSet.flatten_inter self other
-
-let is_must_race threads parallel self other =
-  let base = MemoryAccess.get_base self in
-  let is_precise = MemoryAccess.is_precise self in
-  let is_allocated_array = is_alloced_array base in
-  let are_parallel =
-    ParallelAnalysis.Result.must_run_in_parallel parallel self.callstack other.callstack
-  in
-  Racer.debug ">  surely parallel: %b\n" are_parallel;
-  Racer.debug ">  precise access: %b\n" is_precise;
-  Racer.debug ">  is allocted array: %b\n" is_allocated_array;
-
-  is_race_base threads parallel Lock.PowerSet.flatten_union self other
-  && is_precise
-  && not (Base.is_weak base)
-  && are_parallel
-  && is_unique_malloc self threads base
-
 let of_accesses kind access1 access2 =
   let base = MemoryAccess.get_base access1 in
   let offset1 = MemoryAccess.get_offset access1 in
@@ -98,32 +77,61 @@ let of_accesses kind access1 access2 =
   let inter = Int_Intervals.narrow offset1 offset2 in (* TODO: use meet in must mode *)
   {kind = kind; base = base; offset = inter; accesses = (access1, access2)}
 
-let check threads parallel access1 access2 =
-  if is_must_race threads parallel access1 access2 then
-    Some (of_accesses Must access1 access2)
-  else if is_may_race threads parallel access1 access2 then
-    Some (of_accesses May access1 access2)
-  else None
+let check_must_race threads parallel self other =
+  let base = MemoryAccess.get_base self in
+  let is_precise = MemoryAccess.is_precise self in
+  let is_allocated_array = is_alloced_array base in
+  let are_parallel =
+    ParallelAnalysis.Result.must_run_in_parallel parallel self.callstack other.callstack
+  in
+  let locksets_disjoint = 
+    Lock.Set.rw_disjoint (Lock.PowerSet.flatten_union self.locksets) (Lock.PowerSet.flatten_union other.locksets) 
+  in
+  let not_weak = not @@ Base.is_weak base in
+  let unique_malloc = is_unique_malloc self threads base in
+
+  Racer.debug ">  surely parallel: %b\n" are_parallel;
+  Racer.debug ">  precise access: %b\n" is_precise;
+  Racer.debug ">  is allocted array: %b\n" is_allocated_array;
+
+  if (is_precise && not_weak && are_parallel && unique_malloc && locksets_disjoint) then
+    Option.some @@ of_accesses Must self other
+  else
+    let reason1 = if is_precise then None else Some "not precise" in
+    let reason2 = if not_weak then None else Some "weak" in
+    let reason3 = if are_parallel then None else Some "not parallel" in
+    let reason4 = if unique_malloc then None else Some "not unique malloc" in
+    let reason5 = if locksets_disjoint then None else Some "locksets are not must-disjoint" in
+    let reason = String.concat " & " @@ List.filter_map Fun.id [reason1; reason2; reason3; reason4; reason5] in
+    Option.some @@ of_accesses (May reason) self other
+
+let check threads parallel a1 a2 =
+  if not @@ is_may_race threads parallel a1 a2 then None
+  else check_must_race threads parallel a1 a2
 
 let is_write_write {accesses = (a1, a2); _} =
   a1.kind = Write && a2.kind = Write
 
 let show_kind = function
   | Must -> "must"
-  | May -> "may"
+  | May _ -> "may"
+
+let show_reason race = match race.kind with
+  | Must -> ""
+  | May reason -> "Reason: " ^ reason
 
 let show race =
   if Int_Intervals.is_top race.offset then
-    Format.asprintf "Data race (%s) on %a (weak: %b)"
+    Format.asprintf "Data race (%s) on %a\n%s"
       (show_kind race.kind)
       Base.pretty race.base
-      (Base.is_weak race.base)
+      (show_reason race)
   else
-    Format.asprintf "Data race (%s) on %a (weak: %b) at offset %a"
+    Format.asprintf "Data race (%s) on %a at offset %a\n%s"
       (show_kind race.kind)
       Base.pretty race.base
-      (Base.is_weak race.base)
       Int_Intervals.pretty race.offset
+      (show_reason race)
 
 let report race =
   let a1, a2 = race.accesses in
